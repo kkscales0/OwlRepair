8bf52d6d5bcc0f4ac3a7fcb5255aee7c8eff4507
FireMMDC <mdelcampo2013@fau.edu>
2016-11-10 23:58:21 -0500

    Fixed api call by creating JSON to send int, pulled the Array out of the data object returned from api calls

--- resources/js/getCampusBuilding.js
+++ resources/js/getCampusBuilding.js
@@ -3,11 +3,11 @@ var buildingLists = {};
 
 $(function(){
     $.get("https://owlrepair-148215.appspot.com/api/campus/getAll",function(data,status){
-        console.log(data);
-        campus = data;
+        campus = data.CAMPUSES;
+        console.log(campus);
         var optionsString = "<option hidden >Select Campus</option>";
         $.each(campus,function(key,value){
-            optionsString += "<option value='"+value[0].CAMPUS_ID+"'>"+value[0].CAMPUS_DESC+"</option>";
+            optionsString += "<option value='"+value.CAMPUS_ID+"'>"+value.CAMPUS_DESC+"</option>";
         });
         $("#campusSelect").html(optionsString);
     });
@@ -15,15 +15,12 @@ $(function(){
 
 $("#campusSelect").change(function(){
         var selectedCampus =  parseInt($("#campusSelect").val());
-      $.post("https://owlrepair-148215.appspot.com/api/building/getAllForCampus",selectedCampus,function(data,status){
-        console.log(data);
-          buildingLists[selectedCampus] = data;
+      $.post("https://owlrepair-148215.appspot.com/api/building/getAllForCampus",{ campusId: selectedCampus},function(data,status){
+          buildingLists[selectedCampus] = data.BUILDINGS;
           console.log(buildingLists);
         var optionsString = "<option hidden >Select Building</option>";
         $.each(buildingLists[selectedCampus],function(key,value){
-            if( value.length>0){
-            optionsString += "<option value='"+value[0].BUILDING_ID+"'>"+value[0].BUILDING_DESC+"</option>";
-            }
+            optionsString += "<option value='"+value.BUILDING_ID+"'>"+value.BUILDING_DESC+"</option>";          
         });
         $("#buildingSelect").html(optionsString);
     });
